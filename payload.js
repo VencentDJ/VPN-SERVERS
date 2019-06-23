c5d374e130e9b190406b78eed16dcab40520fdda
VencentDJ <51875892+VencentDJ@users.noreply.github.com>
2019-06-23 19:46:22 +0800

    Add files via upload

---
+++ payload.js
@@ -0,0 +1,99 @@
+//sniff pa gagu mamatay na sana magulang mo
+[{
+      "Name": "SUN TU/CTC PROMO",
+      "Info":" Register to TU promo + ",
+      "Payload":{
+          "Injection": 2,
+          "Method":"POST",
+          "Query":0,
+          "Url":"LCksKSwpLClnZGlgKWhg",
+          "Host":true,
+          "Online-Host":true,
+          "Forward-Host":true,
+          "Reverse-Proxy":false,
+          "Keep-Alive":true
+               
+      }
+  },
+  {
+      "Name":"SMART SAKTO20+ PROMO",
+      "Info":"Register to SAKTO20",
+       "Payload":{
+           "Injection": 2,
+          "Method":"POST",
+          "Query":0,
+          "Url":"cWRdYG0pXmpo",
+          "Host":true,
+          "Online-Host":true,
+          "Forward-Host":true,
+          "Reverse-Proxy":false,
+          "Keep-Alive":true
+      }
+  },
+  {
+      "Name":"TNT ML10 PROMO",
+      "Info":"Register to ML10",
+       "Payload":{
+          "Injection": 2,
+          "Method":"POST",
+          "Query":0,
+       
+"Url":"UmBdKWhqXWRnYGJgaV9uKV5qaClramRpb25faW4pXWRf",
+          "Host":true,
+          "Online-Host":true,
+          "Forward-Host":false,
+          "Reverse-Proxy":false,
+          "Keep-Alive":true
+ 
+  }
+  },
+  {
+      "Name":"GTM FB1D PROMO",
+      "Info":"Register to FB1D",
+       "Payload":{
+          "Injection": 2,
+          "Method":"POST",
+          "Query":0,
+          "Url":"YV1eX2kpXmpo",
+          "Host":true,
+          "Online-Host":true,
+          "Forward-Host":false,
+          "Reverse-Proxy":false,
+          "Keep-Alive":true
+ 
+ 
+           }
+  },
+  {
+      "Name":"GTM MB PROMO",
+      "Info":"Register to GTM MB Promo",
+       "Payload":{
+          "Injection": 2,
+          "Method":"POST",
+          "Query":0,
+          "Url":"cnJyKV9cbWZkb1xnZ2ttanN0KVxra25ram8pXmpo",
+          "Host":true,
+          "Online-Host":true,
+          "Forward-Host":true,
+          "Reverse-Proxy":false,
+          "Keep-Alive":true
+      }
+  },
+  {
+      "Name":"GTM GOWATCH PROMO",
+      "Info":"Register to GOWATCH PROMO",
+       "Payload":{
+          "Injection": 2,
+          "Method":"POST",
+          "Query":0,
+          "Url":"bm5nKWJub1xvZF4pXmpo",
+          "Host":true,
+          "Online-Host":true,
+          "Forward-Host":true,
+          "Reverse-Proxy":false,
+          "Keep-Alive":true
+      }
+  }]
+ }
+  }]
+ }
